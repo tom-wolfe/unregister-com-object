8ac835f0925dc1a1cb25569b30c0e00cf88a11b0
Tom Wolfe <trwolfe13@gmail.com>
2018-06-30 12:59:18 +0100

    Add initial project

---
+++ src/UnregisterComObject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Win32;
+
+namespace UnregisterComObject {
+    static class Program {
+
+        static void Main(string[] args) {
+            if (args.Length != 1) {
+                Console.WriteLine("Please provide the name of the COM object to remove.");
+                return;
+            }
+            var comObjName = args[0];
+
+            var regKey = Registry.ClassesRoot.OpenSubKey(comObjName + @"\CLSID", false);
+            if (regKey == null) {
+                Console.WriteLine("COM object not found.");
+                return;
+            }
+
+            var clsId = regKey.GetValue(string.Empty);
+            Registry.ClassesRoot.DeleteSubKeyTree(comObjName);
+            Registry.ClassesRoot.DeleteSubKeyTree(@"CLSID\" + clsId);
+            Console.WriteLine("COM object uninstalled successfully.");
+        }
+    }
+}
---
+++ src/UnregisterComObject/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+// Review the values of the assembly attributes
+
+[assembly: AssemblyTitle("UnregisterComObject")]
+[assembly: AssemblyDescription("A console application that forcibly unregisters a COM object.")]
+[assembly: AssemblyProduct("Unregister COM Object")]
+
+[assembly: ComVisible(false)]
+
+//The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("477FDC15-B605-4AB6-A425-1713D283BA90")]
